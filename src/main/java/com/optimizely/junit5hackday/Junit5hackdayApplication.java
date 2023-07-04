45232707948440a0ef55a41b2bdd2eef073bfec6
tam pham <tam.pham@optimizely.com>
2023-07-04 13:19:33 +0700

    Base project

---
+++ src/main/java/com/optimizely/junit5hackday/Junit5hackdayApplication.java
@@ -0,0 +1,13 @@
+package com.optimizely.junit5hackday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5hackdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Junit5hackdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optimizely/junit5hackday/Junit5hackdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimizely.junit5hackday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Junit5hackdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
